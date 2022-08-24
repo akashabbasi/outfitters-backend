d240ae372a8acd143ee9e8d0d5b8f191b48159b4
Akash Sabir <akashsabir007@gmail.com>
2022-08-24 17:35:02 +0500

    Add more boilerplate code

---
+++ src/modules/categories/controllers/category.controller.ts
@@ -0,0 +1,14 @@
+import { Controller } from '@nestjs/common';
+
+@Controller({
+
+})
+export class CategoryController {
+  constructor() {}
+
+  async create() {}
+  async findAll() {}
+  async findOne() {}
+  async update() {}
+  async toggleActiveStatus() {}
+}
---
+++ src/modules/categories/schemas/category.schema.ts
@@ -0,0 +1,54 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export const CategoryCollectionName = 'categories';
+
+@Schema({
+  timestamps: true,
+  versionKey: false,
+})
+export class Category {
+  @Prop({
+    required: true,
+    index: true,
+    lowercase: true,
+    trim: true,
+  })
+  name: string;
+
+  @Prop({
+    trim: true,
+  })
+  description: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    trim: true,
+  })
+  slug: string;
+
+  @Prop({
+    required: true,
+    trim: true,
+    match: [
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+      'Invalid image url',
+    ],
+  })
+  imageUrl: string;
+
+  @Prop({
+    default: true,
+  })
+  isActive: boolean;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: CategoryCollectionName,
+  })
+  parentCategory: Category;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
+export type CategoryDocument = Category & Document;
